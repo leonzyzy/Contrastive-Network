7d8b58dec78a6397cc49e18f0179b9ac31927e33
Zhiyuan <li3z3@mail.uc.edu>
2023-05-11 21:39:09 -0400

    Create CSSLoss.py

---
+++ loss/CSSLoss.py
@@ -0,0 +1,22 @@
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+from pytorch_metric_learning import losses
+
+class CSSLoss(nn.Module):
+    def __init__(self, temperature):
+        super(CSSLoss, self).__init__()
+        self.temperature = temperature
+
+    def forward(self, feature_vectors, labels):
+        # Normalize feature vectors
+        feature_vectors_normalized = F.normalize(feature_vectors, p=2, dim=1)
+        # Compute logits
+        logits = torch.div(
+            torch.matmul(
+                feature_vectors_normalized, torch.transpose(feature_vectors_normalized, 0, 1)
+            ),
+            self.temperature,
+        )
+        return losses.NTXentLoss(temperature=self.temperature)(logits, torch.squeeze(labels))
+    
